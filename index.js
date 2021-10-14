5b89a30529c84ff768a2901da6e39f20fa70db6b
Christy Perozzi <christy.perozzi@gmail.com>
2021-10-13 21:59:30 -0400

    commit tree 7530244da17181036a450356a31a1218448babd9

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Your code here
+//as a function
+function saturdayFun (plan = 'roller-skate'){
+    return `This Saturday, I want to ${plan}!`
+};
+
+//as a function expression 
+let mondayWork = function (plan = 'go to the office') {
+    return `This Monday, I will ${plan}.`
+};
+
+
+function wrapAdjective(flair = "*"){
+    return function(adj1 = "special"){
+        return `You are ${flair}${adj1}${flair}!`;
+    }
+};
+
+//let myStr = wrapAdjective("*~*")("rad");
+//console.log(myStr);
+
+
